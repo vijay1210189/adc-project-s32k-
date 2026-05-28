6a8bb9f8bfd037af98332b7aa48578653a207dc2
vijay1210189 <vijaykumar.somasundaram12@gmail.com>
2026-05-28 11:24:48 +0530

    Update adc.h

--- adc.h
+++ adc.h
@@ -1,7 +1,10 @@
 #ifndef ADC_H
 #define ADC_H
 
-/* Step 2: ADC Initialization */
+/* Step 2: Initialization */
 void ADC_Init();
+
+/* Step 3: Start conversion */
+void ADC_Start(int channel);
 
 #endif
