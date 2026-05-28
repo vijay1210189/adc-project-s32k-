49bcd7f42f884475c5570f2b8e71b53a346be3ec
vijay1210189 <vijaykumar.somasundaram12@gmail.com>
2026-05-28 11:27:30 +0530

    Update adc.c

--- adc.c
+++ adc.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include "adc.h"
 
-/* Simulated SC1 register */
+/* Simulated registers */
 int SC1;
+int COCO;  // conversion complete flag
 
-/* Step 2: Initialize ADC */
+/* Step 2 */
 void ADC_Init()
 {
     printf("ADC Initialized\n");
 }
 
-/* Step 3: Start ADC Conversion */
+/* Step 3 + Step 4 combined */
 void ADC_Start(int channel)
 {
-    SC1 = channel;   // Select channel (like ADCH)
+    SC1 = channel;   // select channel
+    COCO = 0;        // not done
 
     printf("ADC Started on Channel: %d\n", channel);
+
+    /* --------- SIMULATION OF CONVERSION --------- */
+
+    // In real hardware → SAR conversion happens here
+    // We just wait (simulate delay)
+    for(int i = 0; i < 1000000; i++);
+
+    /* Conversion done */
+    COCO = 1;
+}
+
+/* Step 4 */
+int ADC_IsDone()
+{
+    return COCO;
 }
