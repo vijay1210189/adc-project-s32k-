738e51f9e354b0c7eb8e0ededaa24cb0ce19f25e
vijay1210189 <vijaykumar.somasundaram12@gmail.com>
2026-05-28 11:25:22 +0530

    Update adc.c

--- adc.c
+++ adc.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include "adc.h"
 
+/* Simulated SC1 register */
+int SC1;
+
 /* Step 2: Initialize ADC */
 void ADC_Init()
 {
     printf("ADC Initialized\n");
+}
+
+/* Step 3: Start ADC Conversion */
+void ADC_Start(int channel)
+{
+    SC1 = channel;   // Select channel (like ADCH)
+
+    printf("ADC Started on Channel: %d\n", channel);
 }
